67ecdaaee59aa62bb479ed8c06218f9e5aaaa368
liangdong <liangdong@smzdm.com>
2017-12-27 13:11:11 +0800

    routing table

--- routing_table.go
+++ routing_table.go
@@ -4,10 +4,12 @@ import (
 	"time"
 	"math/big"
 	"sync"
+	"sort"
 )
 
 const (
  	KNODES = 8	// 每个桶保存8个节点
+ 	MAX_FAIL_TIMES = 3 // 3次连续fail则标记bad
 
  	// 节点状态
 	NODE_STATUS_GOOD = 1
@@ -32,6 +34,29 @@ type RoutingTable struct {
 	mutex sync.Mutex
 }
 
+type ClosestNodes struct {
+	target string
+	nodes []*CompactNode
+}
+
+func (closest ClosestNodes) Len() int {
+	return len(closest.nodes)
+}
+
+func (closest ClosestNodes) Swap(i, j int) {
+	closest.nodes[i], closest.nodes[j] = closest.nodes[j], closest.nodes[i]
+}
+
+func (closest ClosestNodes) Less(i, j int) bool {
+	leftId := nodeId2Int(closest.nodes[i].Id)
+	rightId := nodeId2Int(closest.nodes[j].Id)
+	targetId := nodeId2Int(closest.target)
+
+	// 计算异或距离, 比较大小
+	cmp := new(big.Int).Xor(leftId, targetId).Cmp( new(big.Int).Xor(rightId, targetId) )
+	return cmp < 0
+}
+
 func newBucket(min, max *big.Int) (bucket *Bucket) {
 	bucket = &Bucket{}
 
@@ -42,12 +67,16 @@ func newBucket(min, max *big.Int) (bucket *Bucket) {
 	return
 }
 
+func nodeId2Int(nodeId string) *big.Int {
+	return new(big.Int).SetBytes([]byte(nodeId))
+}
+
 func (bucket *Bucket) size() int {
 	return len(bucket.nodes)
 }
 
 func (bucket *Bucket) inRange(nodeId string) bool {
-	intId := new(big.Int).SetBytes([]byte(nodeId))
+	intId := nodeId2Int(nodeId)
 	return intId.Cmp(bucket.min) >= 0 && intId.Cmp(bucket.max) < 0
 }
 
@@ -93,50 +122,59 @@ func CreateRoutingTable() (rt *RoutingTable) {
 	return
 }
 
-func (rt *RoutingTable) splitBucket() {
-	toSplit := rt.buckets[len(rt.buckets) - 1]
+func (rt *RoutingTable) splitBucket(idx int) {
+	toSplit := rt.buckets[idx]
 
 	sumRange := new(big.Int).Add(toSplit.min, toSplit.max)
 	mid := new(big.Int).Div(sumRange, big.NewInt(2))
 
-	leftBucket := newBucket(toSplit.min, mid)
 	rightBucket := newBucket(mid, toSplit.max)
+	toSplit.max = mid
 
-	// 原桶数据分裂
+	// 原桶分裂成2个新桶
 	for nodeId, node := range toSplit.nodes {
-		if leftBucket.inRange(nodeId) {
-			leftBucket.nodes[nodeId] = node
-		} else {
+		if !toSplit.inRange(nodeId) {
+			delete(toSplit.nodes, nodeId)
 			rightBucket.nodes[nodeId] = node
 		}
 	}
+	rightBucket.lastActive = toSplit.lastActive
 
-	if leftBucket.inRange(MyNodeId()) {
-		leftBucket, rightBucket = rightBucket, leftBucket
+	// 插入分裂后的桶
+	rt.buckets = append(rt.buckets, nil) // 扩容
+	insertIdx := idx + 1
+	copy(rt.buckets[insertIdx + 1:], rt.buckets[insertIdx:])
+	rt.buckets[insertIdx] = rightBucket
+}
+
+func (rt *RoutingTable) findBucket(nodeId string) int {
+	for i := 0; i < len(rt.buckets); i++ {
+		if !rt.buckets[i].inRange(nodeId) {
+			continue
+		}
+		return i
 	}
-	leftBucket.lastActive = toSplit.lastActive
-	rt.buckets[len(rt.buckets) - 1] = leftBucket
-	rt.buckets = append(rt.buckets, rightBucket)
+	return -1
 }
 
 func (rt *RoutingTable) insertNode(nodeInfo *CompactNode) bool {
 	if nodeInfo.Id == MyNodeId() {
 		return true
 	}
-	for i := 0; i < len(rt.buckets); i++ {
-		if !rt.buckets[i].inRange(nodeInfo.Id) {
-			continue
-		}
-		if rt.buckets[i].insertNode(nodeInfo) { // bucket没满插入成功
-			return true
-		}
-		if i + 1 != len(rt.buckets) { // bucket不包含自身,无法分裂
-			return false
-		}
-		rt.splitBucket()
-		return rt.insertNode(nodeInfo)
+
+	idx := rt.findBucket(nodeInfo.Id)
+	if idx < 0 {
+		return false // never reach
 	}
-	return false
+
+	if rt.buckets[idx].insertNode(nodeInfo) { // bucket没满插入成功
+		return true
+	}
+	if !rt.buckets[idx].inRange(MyNodeId()) { // bucket不包含自身,无法分裂
+		return false
+	}
+	rt.splitBucket(idx)
+	return rt.insertNode(nodeInfo)
 }
 
 func (rt *RoutingTable) InsertNode(nodeInfo *CompactNode) bool {
@@ -149,4 +187,76 @@ func (rt *RoutingTable) Size() int {
 	rt.mutex.Lock()
 	defer rt.mutex.Unlock()
 	return len(rt.buckets)
+}
+
+func (rt *RoutingTable) Fail(nodeId string) {
+	rt.mutex.Lock()
+	defer rt.mutex.Unlock()
+
+	if nodeId == MyNodeId() {
+		return
+	}
+
+	idx := rt.findBucket(nodeId)
+	if idx < 0 {
+		return
+	}
+
+	if node, exist := rt.buckets[idx].nodes[nodeId]; exist {
+		if node.status != NODE_STATUS_BAD {
+			node.failTimes++
+			if node.failTimes >= MAX_FAIL_TIMES {
+				node.status = NODE_STATUS_BAD
+			}
+		}
+	}
+}
+
+func (rt *RoutingTable) ClosestNodes(nodeId string) (nodes []*CompactNode) {
+	nodes = make([]*CompactNode, 0)
+
+	rt.mutex.Lock()
+	defer rt.mutex.Unlock()
+
+	idx := rt.findBucket(nodeId)
+	if idx < 0 {
+		return
+	}
+
+	for _, node := range rt.buckets[idx].nodes {
+		if node.info.Id != nodeId {
+			nodes = append(nodes, node.info)
+		}
+	}
+
+	// 不足8个, 找周边的bucket
+	if len(nodes) < KNODES {
+		leftIdx := idx - 1
+		rightIdx := idx + 1
+		for len(nodes) < KNODES && (leftIdx >= 0 || rightIdx < len(rt.buckets)){ // 从左边和右边的邻居桶补一些进来
+			if leftIdx >= 0 {
+				for _, node := range rt.buckets[leftIdx].nodes {
+					nodes = append(nodes, node.info)
+				}
+			}
+			if rightIdx < len(rt.buckets) {
+				for _, node := range rt.buckets[rightIdx].nodes {
+					nodes = append(nodes, node.info)
+				}
+			}
+			leftIdx--
+			rightIdx++
+		}
+	}
+
+	// 按距离排序
+	closestNodes := ClosestNodes{}
+	closestNodes.target = nodeId
+	closestNodes.nodes = nodes
+	sort.Sort(closestNodes)
+	// 取最近的8个
+	if len(nodes) > KNODES {
+		nodes = nodes[:KNODES]
+	}
+	return
 }
--- test/test_routing_table.go
+++ test/test_routing_table.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/owenliang/dht"
+	"fmt"
 )
 
 func main()  {
 	rt := dht.CreateRoutingTable()
+	size := 0
 	for   {
 		rt.InsertNode(&dht.CompactNode{"", dht.GenNodeId()})
+		if size != rt.Size() {
+			size = rt.Size()
+			fmt.Println(size)
+			fmt.Println(rt.ClosestNodes(dht.MyNodeId()))
+		}
 	}
 }
